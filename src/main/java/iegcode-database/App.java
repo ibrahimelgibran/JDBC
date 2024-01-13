f7d305dd63a7b1151d6f94461072c6a94f9fa837
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-13 09:46:18 +0700

    first commit

---
+++ src/main/java/iegcode-database/App.java
@@ -0,0 +1,13 @@
+package iegcode-database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
